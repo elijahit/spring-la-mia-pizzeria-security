0a01e8840dbb0a121381b4b51d2830e48667e602
Elijah <64620855+elijahit@users.noreply.github.com>
2024-07-17 19:12:57 +0200

    ADD: Model User e Role

---
+++ src/main/java/it/elijah/pizzeria/model/Role.java
@@ -0,0 +1,15 @@
+package it.elijah.pizzeria.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+public class Role {
+
+  @Id
+  private Integer id;
+
+  @NotNull
+  private String name;
+}
---
+++ src/main/java/it/elijah/pizzeria/model/User.java
@@ -0,0 +1,26 @@
+package it.elijah.pizzeria.model;
+
+import java.util.Set;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+public class User {
+
+  @Id
+  private Integer id;
+
+  @NotNull
+  private String username;
+
+  @NotNull
+  private String password;
+
+  @ManyToMany(fetch = FetchType.EAGER)
+  private Set<Role> roles;
+  
+}
